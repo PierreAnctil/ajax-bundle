951c61d1e28ff6a23d65b4893e6f1f7ea142dc40
mathieumuller <mathieu.1984@live.fr>
2016-06-28 00:35:32 +0200

    ...

--- DependencyInjection/AxiolabAjaxExtension.php
+++ DependencyInjection/AxiolabAjaxExtension.php
@@ -2,11 +2,13 @@
 
 namespace Axiolab\AjaxBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class AxiolabAjaxExtension extends Extension
+class AxiolabAjaxExtension extends Extension implements PrependExtensionInterface
 {
     public function prepend(ContainerBuilder $container)
     {
