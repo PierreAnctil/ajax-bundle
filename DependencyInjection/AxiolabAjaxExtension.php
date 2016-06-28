f9c4332523cdd54e9a0e8aa42b9800913aa7ab5e
mathieumuller <mathieu.1984@live.fr>
2016-06-28 12:36:01 +0200

    AxiolabAjaxBundle assetic configuration

--- DependencyInjection/AxiolabAjaxExtension.php
+++ DependencyInjection/AxiolabAjaxExtension.php
@@ -2,11 +2,11 @@
 
 namespace Axiolab\AjaxBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 class AxiolabAjaxExtension extends Extension implements PrependExtensionInterface
 {
@@ -14,17 +14,7 @@ class AxiolabAjaxExtension extends Extension implements PrependExtensionInterfac
     {
         $configs = $container->getExtensionConfig($this->getAlias());
         $config = $this->processConfiguration(new Configuration(), $configs);
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__.'/../Resources/config')
-        );
-        $loader->load('services.yml');
 
-        $bundleParameters = [];
-        foreach ($config as $node => $value) {
-            $bundleParameters[$node] = $value;
-        }
-        $container->setParameter('axiolab_ajax', $bundleParameters);
         $bundles = $container->getParameter('kernel.bundles');
         //prepend assetic
         if (true === isset($bundles['AsseticBundle'])) {
@@ -39,35 +29,31 @@ class AxiolabAjaxExtension extends Extension implements PrependExtensionInterfac
 
     protected function configureAssetic(ContainerBuilder $container)
     {
-        $jsPath = '%kernel.root_dir%/../vendor/axiolab/bs-select-bundle/Resources/public/js';
-        $cssPath = '%kernel.root_dir%/../vendor/axiolab/bs-select-bundle/Resources/public/css';
+        $jsPath = '%kernel.root_dir%/../vendor/axiolab/ajax-bundle/Resources/public/js';
+        $cssPath = '%kernel.root_dir%/../vendor/axiolab/ajax-bundle/Resources/public/css';
 
-        $jsConfig = [
-            'axiolab_ajax' => [
-                'inputs' => [
-                    $jsPath.'/libs/jquery-form.min.js',
-                    $jsPath.'/libs/toastr.min.js',
-                    $jsPath.'/AjaxResponse.js',
-                    $jsPath.'/CriteriaTable.js',
-                ],
-                'output' => 'js/axiolabajax.js',
+        $output['javascripts'] = [
+            'inputs' => [
+                $jsPath.'/libs/jquery-form.min.js',
+                $jsPath.'/libs/toastr.min.js',
+                $jsPath.'/AjaxResponse.js',
+                $jsPath.'/CriteriaTable.js',
             ],
+            'output' => 'js/axiolabajax.js',
         ];
 
-        $cssConfig = [
-            'axiolab_ajax' => [
-                'inputs' => [
-                    $cssPath.'/toastr.min.css',
-                ],
-                'output' => 'css/axiolabajax.css',
+        $output['stylesheets'] = [
+            'inputs' => [
+                $cssPath.'/toastr.min.css',
             ],
+            'output' => 'css/axiolabajax.css',
         ];
         foreach ($container->getExtensions() as $name => $extension) {
             switch ($name) {
                 case 'assetic':
                     $container->prependExtensionConfig(
                         $name,
-                        ['assets' => ['javascripts' => $jsConfig, 'stylesheets' => $cssConfig]]
+                        ['assets' => $output]
                     );
                     break;
                 default:
@@ -78,5 +64,20 @@ class AxiolabAjaxExtension extends Extension implements PrependExtensionInterfac
 
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        $loader->load('services.yml');
+
+        $bundleParameters = [];
+        foreach ($config as $node => $value) {
+            $bundleParameters[$node] = $value;
+        }
+        $container->setParameter('axiolab_ajax', $bundleParameters);
     }
 }
