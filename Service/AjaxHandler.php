530d3176122231b7e431707f401d871118368743
mathieu muller <mathieu.muller@axiocode.com>
2017-03-13 12:50:15 +0100

    Add redirection method

--- Service/AjaxHandler.php
+++ Service/AjaxHandler.php
@@ -4,18 +4,20 @@ namespace Axiolab\AjaxBundle\Service;
 
 use Axiolab\AjaxBundle\Model\AjaxResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 class AjaxHandler
 {
-    public function __construct()
+    public function __construct(Session $session)
     {
         $encoders = [new JsonEncoder()];
         $normalizers = [new ObjectNormalizer()];
 
         $this->serializer = new Serializer($normalizers, $encoders);
+        $this->session = $session;
     }
 
     public function jsonResponse($data, $status = AjaxResponse::STATUS_SUCCESS, $notify = false, $messages = [], $options = [])
@@ -52,5 +54,22 @@ class AjaxHandler
     public function renderAndNotify($template, $status, $messages, $options = [])
     {
         return $this->render($template, $status, true, $messages, $options);
+    }
+
+    public function redirect($url, $status = AjaxResponse::STATUS_SUCCESS, $notify = false, $message = '')
+    {
+        $response = new AjaxResponse();
+        $response->setStatus($status)
+            ->setRedirect($url)
+        ;
+
+        if ($notify) {
+            $this->session->getFlashBag()->add(
+                $response->getNotificationStatus(),
+                $message
+            );
+        }
+
+        return new Response($this->serializer->serialize($response, 'json'));
     }
 }
